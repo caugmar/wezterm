6997ebac09c4a34a477c36585c58ffcd87bb83b0
Guto Marcicano <caugmar@gmail.com>
2026-02-03 18:20:26 -0300

    Mais linhas.

--- wezterm.lua
+++ wezterm.lua
@@ -2,11 +2,17 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.initial_cols = 100
-config.initial_rows = 25
+config.initial_rows = 30
 config.font = wezterm.font("FiraCode Nerd Font Mono")
-config.font_size = 14
+config.font_size = 12
 config.color_scheme = "GruvboxDarkHard"
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 4,
+	right = 4,
+	top = 4,
+	bottom = 4,
+}
 
 config.keys = {
 	-- Turn off the default CMD-m Hide action, allowing CMD-m to
